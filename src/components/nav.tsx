f62a8136b21b21d4edfc0d75c2ff92faab88dcae
Zwix-dev <arthur.duval18@gmail.com>
2024-12-05 02:49:11 +0100

    Commit Initial

---
+++ src/components/nav.tsx
@@ -0,0 +1,45 @@
+"use client"
+import React from "react";
+
+export default function Nav() {
+    const [openNav, setOpenNav] = React.useState(false);
+
+    const handleWindowResize = () =>
+        window.innerWidth >= 960 && setOpenNav(false);
+
+    React.useEffect(() => {
+        window.addEventListener("resize", handleWindowResize);
+
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    }, []);
+    return (
+        <nav className=" py-6 shadow-sm">
+            <div className="container mx-auto flex items-center justify-between">
+                <div className="text-xl font-semibold text-indigo-900">
+                Art'Vacati<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-8 inline align-middle text-yellow-400"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" /></svg>n
+                    
+                </div>
+                <div className="flex items-center space-x-6 text-sm text-gray-500">
+                    <a href="#" className="text-indigo-900 font-semibold hover:text-yellow-300">
+                        Home
+                    </a>
+                    <a href="#features" className="text-indigo-900 font-semibold hover:text-yellow-300">
+                        Features
+                    </a>
+                    <a href="#shop" className="text-indigo-900 font-semibold hover:text-yellow-300">
+                        Pricing
+                    </a>
+                </div>
+                <div className="flex items-center space-x-4">
+                    <a href="#signin" className="text-sm text-indigo-900 font-semibold hover:text-yellow-300">
+                        Log In
+                    </a>
+                    <a href="#signup" className="px-4 py-2 text-sm text-white bg-teal-500 rounded-full hover:bg-teal-600 font-semibold">Sign In</a>
+                </div>
+                
+            </div>
+        </nav>
+    );
+}
