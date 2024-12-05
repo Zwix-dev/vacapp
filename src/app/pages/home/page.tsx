816489563933d4d6027c20c5be0b81a2bea5ddeb
Zwix-dev <arthur.duval18@gmail.com>
2024-12-05 02:54:46 +0100

    Fix w

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
                                     <path fillRule="evenodd" d="M20.599 1.5c-.376 0-.743.111-1.055.32l-5.08 3.385a18.747 18.747 0 0 0-3.471 2.987 10.04 10.04 0 0 1 4.815 4.815 18.748 18.748 0 0 0 2.987-3.472l3.386-5.079A1.902 1.902 0 0 0 20.599 1.5Zm-8.3 14.025a18.76 18.76 0 0 0 1.896-1.207 8.026 8.026 0 0 0-4.513-4.513A18.75 18.75 0 0 0 8.475 11.7l-.278.5a5.26 5.26 0 0 1 3.601 3.602l.502-.278ZM6.75 13.5A3.75 3.75 0 0 0 3 17.25a1.5 1.5 0 0 1-1.601 1.497.75.75 0 0 0-.7 1.123 5.25 5.25 0 0 0 9.8-2.62 3.75 3.75 0 0 0-3.75-3.75Z" clipRule="evenodd" />
                                 </svg>
                                 <div className="flex flex-col w-80 pb-10">
-                                    <p className="text-xl font-sans text-slate-500 m-8">Interface</p>
+                                    <p className="text-xl font-sans text-slate-500 m-8">Interfaces</p>
                                     <p className="text-gray-500">Conçu pour offrir une expérience visuelle épurée et élégante. Avec une interface intuitive et un design minimaliste, il met en valeur l'essentiel pour garantir une navigation fluide et agréable.</p>
                                 </div>
                             </div>
