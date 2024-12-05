286aad5cee0d033bcd7db5c555ac612394d62d89
Zwix-dev <arthur.duval18@gmail.com>
2024-12-05 23:32:06 +0100

    fix icons

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -17,7 +17,7 @@ export default function Nav() {
     return (
         <nav className="py-4 shadow-sm">
             <div className="container mx-auto flex items-center justify-between overflow-hidden">
-                <div className="text-xl font-semibold text-indigo-900">
+                <div className="ml-4 text-xl font-semibold text-indigo-900 md:ml-0">
                     Vac'Acti<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8 inline align-middle text-yellow-400"><path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" /></svg>n
                 </div>
                 <div className="hidden md:flex items-center space-x-6 text-sm text-gray-500">
@@ -38,7 +38,7 @@ export default function Nav() {
                     <a href="#signup" className="px-4 py-2 text-sm text-white bg-teal-500 rounded-full hover:bg-teal-600 font-semibold">Sign In</a>
                 </div>
                 <div
-                    className="md:hidden flex items-center cursor-pointer p-2 overflow-x-hidden"
+                    className="md:hidden flex items-center cursor-pointer mr-6 overflow-x-hidden"
                     onClick={() => setOpenNav(!openNav)}
                 >
                     <svg
